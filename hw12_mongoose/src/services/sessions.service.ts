ecf04dd9dd4c920ec45f318ec8a195d6c9fb6afe
zolotukhin_petro <zolotukhinpv@gmail.com>
2025-09-22 20:20:46 +0300

    refactoring

--- hw12_mongoose/src/services/sessions.service.ts
+++
@@ -1,8 +0,0 @@
-import User from "../db/models/User";
-import Session from "../db/models/Session";
-
-export const getAllSessions = async () => {
-  return await Session.findAll({
-    include: { model: User, as: "user" },
-  });
-};
